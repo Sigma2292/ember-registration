38b8296a637025561163ecfad70e728dfe088cfa
sigma bharati <sigmabharati59@gmail.com>
2020-05-08 14:34:06 +0530

    initial commit

---
+++ ember-cli-build.js
@@ -0,0 +1,12 @@
+const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+
+module.exports = function(defaults) {
+  let app = new EmberApp({
+    sassOptions: {
+      extension: 'scss'
+    }
+  });
+
+  //...
+  return app.toTree();
+};
